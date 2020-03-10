084c32ae4238ae04c5ef54101fb14451e567b0ca
troldal <kenneth.balslev@gmail.com>
2020-03-10 22:39:03 +0800

    Test refactoring.

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test_find_all.hpp"
+#include "test_find_all_if.hpp"
+#include "test_find_all_if_not.hpp"
+#include "test_find_all_not_of.hpp"
+#include "test_find_all_of.hpp"
+#include "test_find_first_not_of.hpp"
+#include "test_search_all.hpp"
+#include "test_split.hpp"
